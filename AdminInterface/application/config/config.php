d3ed9abe97b9a2d7092dc74cd8e8d67492b91595
mihkel <mihkel.vilismae@gmail.com>
2016-10-10 16:38:18 +0300

    Changed database config.

--- AdminInterface/application/config/config.php
+++ AdminInterface/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 if (getenv('OPENSHIFT_MYSQL_DB_USERNAME'))
-    $config['base_url'] = 'http://raamatadmin-mihkelvilismae.rhcloud.com/AdminInterface/';
+    $config['base_url'] = 'http://admin-mihkelvilismae.rhcloud.com/AdminInterface/';
 else
     $config['base_url'] = 'http://tvp.local/';
 
