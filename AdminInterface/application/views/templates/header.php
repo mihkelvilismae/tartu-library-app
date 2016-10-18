3faf7a90494cb17b633b477b8f5dbebc3524c6fc
mihkel <mihkel.vilismae@gmail.com>
2016-10-18 23:58:47 +0300

    #26
    Added css

--- AdminInterface/application/views/templates/header.php
+++ AdminInterface/application/views/templates/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="<?=base_url('/public/css/style.css')?>" />
+<!--        <link rel="stylesheet" type="text/css" href="--><?//=base_url('/public/css/style.css')?><!--" />-->
+        <link href="<?=base_url('/public/css/bootstrap.css')?>" rel="stylesheet">
         <title><?=$title?></title>
     </head>
     <body>
