3563c4a8bd50e269da587f2e9c6191ce492da331
mihkel <mihkel.vilismae@gmail.com>
2016-10-19 00:05:21 +0300

    #28 Reverted css

--- AdminInterface/application/views/templates/header.php
+++ AdminInterface/application/views/templates/header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-<!--        <link rel="stylesheet" type="text/css" href="--><?//=base_url('/public/css/style.css')?><!--" />-->
-        <link href="<?=base_url('/public/css/bootstrap.css')?>" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="<?=base_url('/public/css/style.css')?>" />
+<!--        <link href="--><?//=base_url('/public/css/bootstrap.css')?><!--" rel="stylesheet">-->
         <title><?=$title?></title>
     </head>
     <body>
