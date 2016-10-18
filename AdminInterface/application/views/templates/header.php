1bbfbbb477c81f42595a777e1dccbefed4e904c7
mihkel <mihkel.vilismae@gmail.com>
2016-10-19 00:31:32 +0300

    #26 - CSS

--- AdminInterface/application/views/templates/header.php
+++ AdminInterface/application/views/templates/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-<!--        <link rel="stylesheet" type="text/css" href="--><?//=base_url('/public/css/style.css')?><!--" />-->
-        <link href="<?=base_url('/public/css/bootstrap.css')?>" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="<?=base_url('/public/css/style.css')?>" />
+<!--        <link href="--><?//=base_url('/public/css/bootstrap.css')?><!--" rel="stylesheet">-->
         <title><?=$title?></title>
     </head>
     <body>
         <h1><?= $title?></h1>
-        <div class="container">
+        <div class="container outer">
+            <div class="container inner">
