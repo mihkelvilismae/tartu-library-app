a12e319719854567d9acdd17a97c222f26291225
mihkel <mihkel.vilismae@gmail.com>
2016-10-19 12:49:28 +0300

    #27 - admininterface

--- AdminInterface/application/views/templates/sidebar.php
+++ AdminInterface/application/views/templates/sidebar.php
@@ -11,7 +11,7 @@
                 <td><a href="<?=base_url("Klassid")?>">Klassid</a></td>
             </tr>
             <tr>
-                <td><a href="<?=base_url("Nimekiri")?>">Raamatunimekirjad</a></td>
+                <td><a href="<?=base_url("Nimekiri")?>">Nimekirjad</a></td>
             </tr>
         </table>
     </span>
