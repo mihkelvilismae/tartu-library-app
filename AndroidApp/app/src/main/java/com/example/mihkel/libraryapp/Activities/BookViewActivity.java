8b6dc6e1637c6fe7ecda6864e8ea76402eab4a10
mihkel <mihkel.vilismae@gmail.com>
2016-11-28 14:07:11 +0200

    #46

---
+++ AndroidApp/app/src/main/java/com/example/mihkel/libraryapp/Activities/BookViewActivity.java
@@ -0,0 +1,15 @@
+package com.example.mihkel.libraryapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mihkel.libraryapp.R;
+
+public class BookViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_view);
+    }
+}
